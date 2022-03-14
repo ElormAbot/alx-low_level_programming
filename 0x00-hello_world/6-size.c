4f52789414b228c9cf0310a14f4a94bc37dfe2ba
ElormAbot <abotsiviaelorm@gmail.com>
2022-03-14 13:28:03 -0700

    Hello, puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char v;
+	int w;
+	long int x;
+	long long int y;
+	float z;
+	printf("Size of a char: %d byte(s)\n", sizeof(v));
+	printf("Size of an int: %d byte(s)\n", sizeof(w));
+	printf("Size of a long int: %d byte(s)\n", sizeof(x));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(y));
+	printf("Size of a float: %d byte(s)\n", sizeof(z));
+	return (o);
+}
+
